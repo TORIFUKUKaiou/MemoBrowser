f67d716073c1f01bd673a705c7202714a52aa70b
TORIFUKU kaiou <torifuku.kaiou@gmail.com>
2013-05-21 10:56:57 +0900

    update AdActivity layout.

--- src/jp/torifuku/memobrowser/AdActivity.java
+++ src/jp/torifuku/memobrowser/AdActivity.java
@@ -2,25 +2,51 @@ package jp.torifuku.memobrowser;
 
 import jp.torifuku.util.torifukuutility.log.TorifukuLog;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * AdActivity
+ * 
  * @author torifuku kaiou
- *
+ * 
  */
 public class AdActivity extends Activity {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		TorifukuLog.methodIn();
-		
+
 		super.onCreate(savedInstanceState);
-		super.setContentView(R.layout.ad_layout);
-		
+		super.setContentView(R.layout.activity_ad);
+
+		Button button = (Button) super.findViewById(R.id.setting_button);
+		button.setOnClickListener(new OnClickListener() {
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see android.view.View.OnClickListener#onClick(android.view.View)
+			 */
+			@Override
+			public void onClick(View v) {
+				TorifukuLog.methodIn();
+
+				Intent intent = new Intent(AdActivity.this,
+						MySettingActivity.class);
+				AdActivity.super.startActivity(intent);
+
+				TorifukuLog.methodOut();
+			}
+		});
+
 		TorifukuLog.methodOut();
 	}
 }
